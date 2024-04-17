1d3e6d5347c7a4b5388d23a3b9fea786ed47f3d1
Fernando Carvalho <nando_carvalhoo@hotmail.com>
2024-04-17 16:58:21 -0300

    wallet controller fully tested

--- src/wallet/wallet.controller.spec.ts
+++ src/wallet/wallet.controller.spec.ts
@@ -15,6 +15,11 @@ describe('WalletController', () => {
           provide: WalletService,
           useValue: {
             create: jest.fn().mockResolvedValue({}),
+            findOne: jest.fn().mockResolvedValue({}),
+            findAll: jest.fn().mockResolvedValue([]),
+            update: jest.fn().mockResolvedValue({}),
+            remove: jest.fn().mockResolvedValue({}),
+            removeAll: jest.fn().mockResolvedValue({}),
           },
         },
       ],
@@ -30,6 +35,8 @@ describe('WalletController', () => {
       currency: 'USD',
       name: 'My Wallet',
       user_id: '123',
+      create_at: new Date(20 - 10 - 2003),
+      updated_at: new Date(26 - 11 - 2008)
     };
 
     const createdWallet = {
@@ -45,12 +52,56 @@ describe('WalletController', () => {
     const result = await walletController.create(walletData as Wallet);
 
     expect(walletService.create).toHaveBeenCalledWith(walletData);
-    expect(result).toEqual(createdWallet);
+    expect(result).toEqual(createdWallet);;
+  });
 
-    //Here I'll do test for create_at and updated_at only to have 100% of test coverage for the entity Wallet
-    expect(result.create_at).toBeDefined();
-    expect(result.create_at).toBeInstanceOf(Date);
-    expect(result.updated_at).toBeDefined();
-    expect(result.updated_at).toBeInstanceOf(Date);
+  it('should return all wallets', async () => {
+    const wallet: Wallet = {
+      id: '1',
+      active: true,
+      currency: 'USD',
+      name: 'My Wallet',
+      user_id: '123',
+      create_at: new Date(),
+      updated_at: new Date(),
+    };
+    const result: Wallet[] = [wallet]; // Adicione a wallet ao array
+
+    jest.spyOn(walletService, 'findAll').mockResolvedValue(result);
+
+    expect(await walletController.findAll()).toBe(result);
+  });
+
+  describe('findOne', () => {
+    it('should return a wallet', async () => {
+      const result = { id: '1', name: 'Test Wallet' };
+      jest.spyOn(walletService, 'findOne').mockImplementation(() => Promise.resolve(result as Wallet));
+
+      expect(await walletController.findOne('1')).toBe(result);
+    });
+  });
+  describe('update', () => {
+    it('should return an updated wallet', async () => {
+      const result = { id: '1', name: 'Updated Wallet' };
+      jest.spyOn(walletService, 'update').mockImplementation(() => Promise.resolve(result as Wallet));
+
+      expect(await walletController.update('1', { name: 'Updated Wallet' })).toBe(result);
+    });
+  });
+
+  describe('remove', () => {
+    it('should return void', async () => {
+      jest.spyOn(walletService, 'remove').mockImplementation(() => Promise.resolve());
+
+      expect(await walletController.remove('1')).toBeUndefined();
+    });
+  });
+
+  describe('removeAll', () => {
+    it('should return void', async () => {
+      jest.spyOn(walletService, 'removeAll').mockImplementation(() => Promise.resolve());
+
+      expect(await walletController.removeAll()).toBeUndefined();
+    });
   });
 });
